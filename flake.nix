81735a6fed7f4c93c5fb61db84ec574362df95a8
r3s1stanc3 <r3s1stanc3@riseup.net>
2023-10-04 15:14:39 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Shaker";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+    (system:
+
+     let
+      pkgs = nixpkgs.legacyPackages.${system};
+      name = "r3s1stanc3";
+      email = "r3s1stanc3@riseup.net";
+     in
+     {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          zola
+          #minify
+        ];
+
+        # overwrite git parameters for anonymity
+        GIT_AUTHOR_NAME = name;
+        GIT_AUTHOR_EMAIL =  email;
+        GIT_COMMITTER_NAME = name;
+        GIT_COMMITTER_EMAIL = email;
+        GIT_SSH_COMMAND = "ssh -i ~/.ssh/id_ed25519_r3s";
+
+        GIT_COMMIT_GPGSIGN = "false";
+        GIT_CONFIG_COUNT = 1;
+        GIT_CONFIG_KEY_0 = "commit.gpgSign";
+        GIT_CONFIG_VALUE_0 = "false";
+      };
+     }
+    );
+}
+#devShells.default = import ./shell.nix { inherit pkgs; };
